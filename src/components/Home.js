0e9d0ced0c4e1e859b3b1fc1a98e35be5616e58f
rakeshdr543 <rakeshdr543@gmail.com>
2020-12-12 13:12:41 +0530

    posts added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import LeaveImage from '../images/pexels-aditya-aiyar-1407305.jpg';
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <main>
+        <img
+          src={LeaveImage}
+          alt="Monstera Leaves"
+          className="absolute object-cover w-full h-full"
+        />
+        <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+          <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+            Aloha. I'm Rakesh D R.
+          </h1>
+        </section>
+      </main>
     )
 }
 
